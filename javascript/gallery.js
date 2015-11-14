c2002555c6b5f39439d4d663287affa83fd8bd87
mstefaniuk <marcin@stefaniuk.info>
2015-11-14 22:54:48 +0100

    Updates

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,24 +1,20 @@
 define(['jquery'], function($) {
-  var resources = [
-    "clust.gv",
-    "clust1.gv",
-    "clust2.gv",
-    "clust3.gv",
-    "clust4.gv",
-    "clust5.gv"
-  ];
 
   return {
     middleware: {
       load: function(req, event, next) {
-        $.get("/gallery/" + req.params.diagram).done(function(diagram) {
+        $.get("/gallery/" + req.params.gallery).done(function(diagram) {
           req.source = diagram;
+          req.document = {
+            type: "fiddle",
+            fork: "gallery/" + req.params.gallery
+          };
           next();
         });
       }
     },
     random: function() {
-      return resources[Math.floor(Math.random() * resources.length)];
+      return "abstract.gv";
     }
   }
 });
