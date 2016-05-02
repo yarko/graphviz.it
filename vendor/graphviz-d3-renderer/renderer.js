ec1e2dddf230813f6178ebcd8daec1f637257b30
mstefaniuk <marcin@stefaniuk.info>
2016-05-02 22:47:56 +0200

    Updates

--- vendor/graphviz-d3-renderer/renderer.js
+++ vendor/graphviz-d3-renderer/renderer.js
@@ -109,7 +109,7 @@ define('transitions/default',[], function() {
   };
 });
 define('stage',["d3", "palette", "transitions/default"], function (d3, palette, defaults) {
-    var svg, main;
+    var svg, main, zoom;
     var order = {
       digraph: 0,
       subgraph: 1,
@@ -120,8 +120,8 @@ define('stage',["d3", "palette", "transitions/default"], function (d3, palette, 
 
     function calculateSizes(main) {
       var margin = 4,
-        boundingWidth = main.shapes[0].points[2][0] + margin*2,
-        boundingHeight = main.shapes[0].points[2][1] + margin*2,
+        boundingWidth = main.shapes[0].points[2][0] + margin * 2,
+        boundingHeight = main.shapes[0].points[2][1] + margin * 2,
         htranslate = main.shapes[0].points[2][1] + margin,
         vtranslate = margin,
         size = main.size || [boundingWidth, boundingHeight];
@@ -143,7 +143,7 @@ define('stage',["d3", "palette", "transitions/default"], function (d3, palette, 
       };
     }
 
-    var labelAttributer = function() {
+    var labelAttributer = function () {
       this
         .attr("x", function (d) {
           return d.x;
@@ -151,31 +151,76 @@ define('stage',["d3", "palette", "transitions/default"], function (d3, palette, 
         .attr("y", function (d) {
           return -d.y;
         })
-        //.attr("text-anchor","middle")
-        //.attr("style", function(d) {
-        //  return d.style.map(
-        //    function(e){
-        //      return [e.key,e.value].join(':');
-        //    }).join(';');
-        //})
         .text(function (d) {
           return d.text;
         });
+
+      this.each(function (d) {
+        var self = d3.select(this);
+        d.style.map(function (e) {
+          switch (e.key) {
+            case "stroke":
+              return {key: "color", value: e.value};
+            case "font-size":
+              return {key: e.key, value: e.value + "px"};
+            default:
+              return e;
+          }
+        }).forEach(function (e) {
+          self.style(e.key, e.value);
+        });
+      });
     };
 
+    function zoomed() {
+      svg.select("g")
+        .attr('transform', 'translate(' + d3.event.translate + ') scale(' + d3.event.scale + ')');
+    }
+
     return {
-      init: function (element) {
+      init: function (definition) {
+        var element = definition.element || definition;
         svg = d3.select(element).append("svg")
           .attr("version", 1.1)
           .attr("xmlns", "http://www.w3.org/2000/svg");
         svg.append("style")
           .attr("type", "text/css")
-          .text('path {fill: transparent} text {text-anchor: middle; font-family:"Times-Roman",serif; font-size: 10pt}');
+          .text([
+            'path {fill: transparent}',
+            'text {text-anchor: middle; font-family:"Times-Roman",serif; font-size: 10pt}',
+            '.overlay {fill: none; pointer-events: all}'
+          ].join(' '));
         svg.append("polygon").attr("stroke", "none");
-        main = svg.append("g");
+        main = svg.append("g").append("g");
+
+        if (definition.zoom) {
+          var extent = definition.zoom && definition.zoom.extent || [0.1, 10];
+          zoom = d3.behavior
+            .zoom()
+            .scaleExtent(extent)
+            .on("zoom", zoomed);
+
+          svg.select("g")
+            .call(zoom)
+            .append("rect")
+            .attr("class", "overlay");
+        }
       },
-      svg: function () {
+      svg: function (reset) {
+        if (reset) {
+          var g = svg.select("g").select("g");
+          if (g[0]) {
+            zoom.scale(1);
+            zoom.translate([0, 0]);
+            g.attr("transform", "translate(0,0)scale(1)");
+          }
+        }
         return svg.node().parentNode.innerHTML;
+      },
+      setZoom: function(zoomParams) {
+        zoomParams.scale && zoom.scale(zoomParams.scale);
+        zoomParams.translate && zoom.translate(zoomParams.translate);
+        zoom.event(svg);
       },
       transitions: function (custom) {
         if (custom) {
@@ -188,7 +233,7 @@ define('stage',["d3", "palette", "transitions/default"], function (d3, palette, 
         var sizes = calculateSizes(stage.main);
         var area = [0, 0, sizes.width, sizes.height];
 
-        transitions.document(svg, function() {
+        transitions.document(svg, function () {
           this
             .attr("width", sizes.width + "pt")
             .attr("height", sizes.height + "pt")
@@ -199,20 +244,29 @@ define('stage',["d3", "palette", "transitions/default"], function (d3, palette, 
         });
 
         var polygon = svg.select("polygon");
-        transitions.canvas(polygon, function() {
+        transitions.canvas(polygon, function () {
           this
             .attr("points", function () {
-              return [[0,0],[0,sizes.height],[sizes.width,sizes.height],[sizes.width,0]]
+              return [[0, 0], [0, sizes.height], [sizes.width, sizes.height], [sizes.width, 0]]
                 .map(function (e) {
                   return e.join(",");
                 }).join(" ");
             });
           var self = this;
-          sizes.style.forEach(function(e) {
+          sizes.style.forEach(function (e) {
             self.style(e.key, e.value);
           });
         });
 
+        var overlay = svg.select("rect.overlay");
+        if (overlay[0]) {
+          transitions.canvas(overlay, function () {
+            this
+              .attr('width', sizes.width / sizes.scaleWidth)
+              .attr('height', sizes.height / sizes.scaleHeight)
+              .attr('y', -sizes.height / sizes.scaleHeight);
+          });
+        }
         var label = main.selectAll("text")
           .data(stage.main.labels);
         label.enter().append("text");
@@ -230,13 +284,13 @@ define('stage',["d3", "palette", "transitions/default"], function (d3, palette, 
           return d.id;
         });
 
-        transitions.nodes(entering.filter(".node"), function() {
+        transitions.nodes(entering.filter(".node"), function () {
           this.style("opacity", 1.0);
         });
-        transitions.relations(entering.filter(".relation"), function() {
+        transitions.relations(entering.filter(".relation"), function () {
           this.style("opacity", 1.0);
         });
-        transitions.exits(groups.exit(), function() {
+        transitions.exits(groups.exit(), function () {
           this.remove();
         });
 
@@ -251,7 +305,7 @@ define('stage',["d3", "palette", "transitions/default"], function (d3, palette, 
           }
         );
         shapes.enter().append("path");
-        transitions.shapes(shapes, function() {
+        transitions.shapes(shapes, function () {
           this
             .attr("d", function (d) {
               var shape = d.shape;
@@ -269,6 +323,31 @@ define('stage',["d3", "palette", "transitions/default"], function (d3, palette, 
         });
         labels.enter().append("text");
         transitions.labels(labels, labelAttributer);
+      },
+      getImage: function(reset) {
+        reset = reset===undefined ? true : reset;
+        var svgXml = this.svg(reset);
+        var scaleFactor = 1;
+
+        var svgImage = new Image();
+        svgImage.src = "data:image/svg+xml;utf8," + svgXml;
+
+        var pngImage = new Image();
+
+        svgImage.onload = function() {
+          var canvas = document.createElement("canvas");
+          canvas.width = svgImage.width * scaleFactor;
+          canvas.height = svgImage.height * scaleFactor;
+
+          var context = canvas.getContext("2d");
+          context.drawImage(svgImage, 0, 0, canvas.width, canvas.height);
+
+          pngImage.src = canvas.toDataURL("image/png");
+          pngImage.width = svgImage.width;
+          pngImage.height = svgImage.height;
+        };
+
+        return pngImage;
       }
     };
   }
@@ -320,7 +399,7 @@ define('renderer',["stage", "worker!layout-worker.js"], function(stage, worker) 
 
   return {
     init: function(element) {
-      stage.init(element);
+      return stage.init(element);
     },
     render: function(source) {
       if (initialized) {
@@ -329,36 +408,7 @@ define('renderer',["stage", "worker!layout-worker.js"], function(stage, worker) 
         pending = source;
       }
     },
-    getImage: function() {
-      var svgXml = stage.svg();
-      var scaleFactor = 1;
-
-      if ("devicePixelRatio" in window) {
-        if (window.devicePixelRatio > 1) {
-          scaleFactor = window.devicePixelRatio;
-        }
-      }
-
-      var svgImage = new Image();
-      svgImage.src = "data:image/svg+xml;utf8," + svgXml;
-
-      var pngImage = new Image();
-
-      svgImage.onload = function() {
-        var canvas = document.createElement("canvas");
-        canvas.width = svgImage.width * scaleFactor;
-        canvas.height = svgImage.height * scaleFactor;
-
-        var context = canvas.getContext("2d");
-        context.drawImage(svgImage, 0, 0, canvas.width, canvas.height);
-
-        pngImage.src = canvas.toDataURL("image/png");
-        pngImage.width = svgImage.width;
-        pngImage.height = svgImage.height;
-      };
-
-      return pngImage;
-    },
+    stage: stage,
     errorHandler: function(handler) {
       callback = handler;
     }
